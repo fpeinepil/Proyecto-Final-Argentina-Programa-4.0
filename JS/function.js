e2487d4b342871fc1303d0c95808162362479f2b
Facundo Peinepil <peinepil@gmail.com>
2023-01-25 15:02:12 -0300

    agregado de js

---
+++ JS/function.js
@@ -0,0 +1,4 @@
+document.getElementById('botonclick').addEventListener('click', function() {
+    console.log("Hola mundo desde EventListener");
+    document.getElementById('hola').innerHTML = "Bienvenido a mi CV";
+});
