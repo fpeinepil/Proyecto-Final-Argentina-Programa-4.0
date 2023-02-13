3502282fb6f353adfe9e976a15a37e0cc87f865c
Facundo Peinepil <peinepil@gmail.com>
2023-02-13 20:56:27 -0300

    escritura de maquina

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,43 @@ const navMenu = document.querySelector(".nav-menu"); /* defino las constantes */
 navBarra.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu-visible"); /*a la barra al hacerle click le asigno la variavle nav-menu-visible que me posiciona el menu en el medio*/
 });
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["dificil","divertido","un trabajo", "una vida"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+
+function type(){
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray(textArrayIndex).charAt (charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, newTextDelay);       
+    }
+}
+function erase() {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
