a67558e13dd6be55747747d7186cd2173d14bc0e
Facundo Peinepil <peinepil@gmail.com>
2023-02-27 22:17:34 -0300

    progreso en el proyecto

--- JS/index.js
+++ JS/index.js
@@ -18,7 +18,8 @@ let charIndex = 0;
 
 function type(){
     if (charIndex < textArray[textArrayIndex].length) {
-        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.
+        add("typing");
         typedTextSpan.textContent += textArray(textArrayIndex).charAt (charIndex);
         charIndex++;
         setTimeout(type, typingDelay);
@@ -29,11 +30,14 @@ function type(){
 }
 function erase() {
     if (charIndex > 0) {
-        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-        typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex - 1);
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.
+        add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0,
+        charIndex - 1);
         charIndex--;
         setTimeout(erase, erasingDelay);
-    } else {
+    }
+    else {
         cursorSpan.classList.remove("typing");
         textArrayIndex++;
         if (textArrayIndex >= textArray.length) textArrayIndex = 0;
@@ -42,5 +46,5 @@ function erase() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    if (textArray.length) setTimeout(type, newTextDelay + 250);
+    if (textArray.length) setTimeout(type, newTextDelay + 1000000);
 });
